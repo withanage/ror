42f611766f7d0c5cf694d2357028bb7fd943963c
withanaged <dulip.withanage@tib.eu>
2020-08-21 17:37:17 +0200

    init

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+require_once('rorPlugin.inc.php');
+return new rorPlugin();
---
+++ rorPlugin.inc.php
@@ -0,0 +1,42 @@
+<?php
+import('lib.pkp.classes.plugins.GenericPlugin');
+
+/**
+ * Class rorPlugin
+ */
+class rorPlugin extends GenericPlugin {
+	public function register($category, $path, $mainContextId = NULL) {
+
+		// Register the plugin even when it is not enabled
+		$success = parent::register($category, $path);
+
+		if ($success && $this->getEnabled()) {
+			// Do something when the plugin is enabled
+		}
+
+		return $success;
+	}
+
+	/**
+	 * @copydoc Plugin::getName()
+	 */
+	function getName() {
+		return 'rorPlugin';
+	}
+
+	/**
+	 * @copydoc Plugin::getDisplayName()
+	 */
+	function getDisplayName() {
+		return __('plugins.generic.ror.displayName');
+	}
+
+	/**
+	 * @copydoc Plugin::getDescription()
+	 */
+	function getDescription() {
+		return __('plugins.generic.ror.description');
+	}
+
+
+}
